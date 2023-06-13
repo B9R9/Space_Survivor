de0982323f20dfdea4fa6fac8ba082d16f49ee27
B9R9 <baptiste.riffard@gmail.com>
2023-06-13 08:43:37 +0300

    Add brouillon game

--- C_Files/gameRun.c
+++ C_Files/gameRun.c
@@ -17,94 +17,94 @@ static e_bool loopGame(u_int32_t gameStep)
 // 	return (height);
 // }
 
-// static void animBackground(SDL_Renderer *render, SDL_Texture *background, int *index)
-// {
-// 	int			maxTextureW;
-// 	int			maxTextureH;
-// 	SDL_Rect	src;
-// 	SDL_Rect	dst;
-
-
-// 	SDL_QueryTexture(background, NULL, NULL, &maxTextureW, &maxTextureH);
-// 	if (*index >= maxTextureW)
-// 		*index = 0;
-// 	src = defineRect(*index, 0, 5, maxTextureH);
-// 	dst = defineRect(-1, 0, 5, SCREEN_H);
-// 	while (dst.x < SCREEN_W)
-// 	{
-// 		SDL_RenderCopy(render, background, &src, &dst);
-// 		src.x += 2;
-// 		if (src.x >= maxTextureW)
-// 			src.x = 0;
-// 		dst.x += 2;
-// 	}
-// 	*index += 2;
-// }
-
-static t_textureToLi *animBackgroundV2(SDL_Renderer *render, SDL_Texture *background)
+static void animBackground(SDL_Renderer *render, SDL_Texture *background, int *index)
 {
-	t_textureToLi	*new;
-	t_textureToLi	*node;
-	t_textureToLi	*temp;
 	int			maxTextureW;
 	int			maxTextureH;
-	int			sizeLi;
-	int			index;
-	int			speed;
 	SDL_Rect	src;
 	SDL_Rect	dst;
 
-	new = NULL;
-	node = NULL;
-	sizeLi = 0;
-	index = 0;
-	speed = 5;
+
 	SDL_QueryTexture(background, NULL, NULL, &maxTextureW, &maxTextureH);
-	while (index <= maxTextureW)
+	if (*index >= maxTextureW)
+		*index = 0;
+	src = defineRect(*index, 0, 5, 100);
+	dst = defineRect(-1, 0, 5, SCREEN_H);
+	while (dst.x < SCREEN_W)
 	{
-		node = (t_textureToLi *)malloc(sizeof(t_textureToLi));
-		if (node == NULL)
-			EXIT_FAILURE;
-		src = defineRect(index, 0, speed, maxTextureH);
-		dst = defineRect(-1, 0, speed, SCREEN_H);
-		node->texture = SDL_CreateTexture(render, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, SCREEN_W, SCREEN_H);
-		SDL_SetTextureBlendMode(node->texture, SDL_BLENDMODE_BLEND);
-		SDL_SetRenderTarget(render, node->texture);
-		node->id = index;
-		while (dst.x < SCREEN_W)
-		{
-			SDL_RenderCopy(render, background, &src, &dst);
-			src.x += speed;
-			if (src.x >= maxTextureW)
-				src.x = 0;
-			dst.x += speed;
-		}
-		SDL_SetRenderTarget(render, NULL);
-		index += speed;
-		sizeLi += 1;
-		node->sizeLi = sizeLi;
-		node->next = NULL;
-		if (new == NULL)
-			new = node;
-		else
-		{
-			temp = new;
-			while (temp->next != NULL)
-				temp = temp->next;
-			temp->next = node;
-		}
+		SDL_RenderCopy(render, background, &src, &dst);
+		src.x += 2;
+		if (src.x >= maxTextureW)
+			src.x = 0;
+		dst.x += 2;
 	}
-	temp = new;
-	while (temp->next != NULL)
-		temp = temp->next;
-	temp->next = new;
-	return (new);
+	*index += 2;
 }
+
+// static t_textureToLi *animBackgroundV2(SDL_Renderer *render, SDL_Texture *background)
+// {
+// 	t_textureToLi	*new;
+// 	t_textureToLi	*node;
+// 	t_textureToLi	*temp;
+// 	int			maxTextureW;
+// 	int			maxTextureH;
+// 	int			sizeLi;
+// 	int			index;
+// 	int			speed;
+// 	SDL_Rect	src;
+// 	SDL_Rect	dst;
+
+// 	new = NULL;
+// 	node = NULL;
+// 	sizeLi = 0;
+// 	index = 0;
+// 	speed = 5;
+// 	SDL_QueryTexture(background, NULL, NULL, &maxTextureW, &maxTextureH);
+// 	while (index <= maxTextureW)
+// 	{
+// 		node = (t_textureToLi *)malloc(sizeof(t_textureToLi));
+// 		if (node == NULL)
+// 			EXIT_FAILURE;
+// 		src = defineRect(index, 0, speed, maxTextureH);
+// 		dst = defineRect(-1, 0, speed, SCREEN_H);
+// 		node->texture = SDL_CreateTexture(render, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, SCREEN_W, SCREEN_H);
+// 		SDL_SetTextureBlendMode(node->texture, SDL_BLENDMODE_BLEND);
+// 		SDL_SetRenderTarget(render, node->texture);
+// 		node->id = index;
+// 		while (dst.x < SCREEN_W)
+// 		{
+// 			SDL_RenderCopy(render, background, &src, &dst);
+// 			src.x += speed;
+// 			if (src.x >= maxTextureW)
+// 				src.x = 0;
+// 			dst.x += speed;
+// 		}
+// 		SDL_SetRenderTarget(render, NULL);
+// 		index += speed;
+// 		sizeLi += 1;
+// 		node->sizeLi = sizeLi;
+// 		node->next = NULL;
+// 		if (new == NULL)
+// 			new = node;
+// 		else
+// 		{
+// 			temp = new;
+// 			while (temp->next != NULL)
+// 				temp = temp->next;
+// 			temp->next = node;
+// 		}
+// 	}
+// 	temp = new;
+// 	while (temp->next != NULL)
+// 		temp = temp->next;
+// 	temp->next = new;
+// 	return (new);
+// }
 
 static void buildBackground(t_game *data, SDL_Renderer *render)
 {
-	// animBackground(render,data->background, &data->indexBackground);
-	data->backgroundView = animBackgroundV2(render,data->background);
+	animBackground(render,data->background, &data->indexBackground);
+	// data->backgroundView = animBackgroundV2(render,data->background);
 	/*build le fond avant de mettre le player, le shooting ou ennemie*/
 }
 
@@ -144,9 +144,9 @@ e_bool gameRun(SDL_Renderer *render, u_int32_t param, u_int32_t *gameStep)
 		checkEventGame(gameStep, &data.direction);
 		printf(" %d || %d || %d || %d\n", data.direction.up, data.direction.down, data.direction.left, data.direction.right );
 		// printf("GAME SECTION: Check passage game event checked\n");
-		// buildBackground(&data, render);
-		SDL_RenderCopy(render, data.backgroundView->texture, NULL, NULL);
-		data.backgroundView = data.backgroundView->next;
+		buildBackground(&data, render);
+		// SDL_RenderCopy(render, data.backgroundView->texture, NULL, NULL);
+		// data.backgroundView = data.backgroundView->next;
 		playerPosition(render, &player, data.direction);
 		// printf("GAME SECTION: Value index --> %d\n", data.indexBackground);
 		// printf("GAME SECTION: Check passage game background builded\n");
